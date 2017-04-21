22766f2a44c017636e257ab9fa974911cdcfb002
Xiunan Yu <yuxi01@gettysburg.edu>
2017-04-21 15:28:18 +0200

    Main Menu

---
+++ SpaceCrane/Assets/Scripts/Xiunan's Scripts/ProtalControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProtalControl : MonoBehaviour {
+
+    public int numCargos;
+
+    bool canEntered;
+
+    //Store the information which how many cargos are collected
+    bool[] actives;
+
+    void Start()
+    {
+        actives = new bool[numCargos];    
+    }
+
+    public void ActiviteOneOfCargoHolder()
+    {
+        for(int i = 0; i < actives.Length; i++)
+        {
+            if(!actives[i])
+            {
+                actives[i] = true;
+                canEntered = CheckAllActived();
+                return;
+            }
+        }
+    }
+
+    bool CheckAllActived()
+    {
+        foreach(bool active in actives)
+        {
+            if(!active)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(canEntered && collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
