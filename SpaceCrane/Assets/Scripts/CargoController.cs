7af7dc7a028e17a83b18d59986f2d92b525c5e5c
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2017-04-21 14:59:45 +0200

    Not working ship controller and cargo controller

---
+++ SpaceCrane/Assets/Scripts/CargoController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Controls pick-uppable objects 
+ * Requires a blank spawner object at the 'home base' of the cargo
+ */
+public class CargoController : MonoBehaviour {
+
+	public GameObject cargoGoalSpace;
+	private Rigidbody rb;
+	private Vector3 initialPosition;
+	private Quaternion initialRotation;
+	private bool isAchieved;
+	private bool isPickupable;
+
+	// Use this for initialization
+	void Start () {
+		isPickupable = true;
+		initialPosition = gameObject.transform.position;
+		initialRotation = gameObject.transform.rotation;
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	public void respawnCargo(){
+		if (!isAchieved) {
+			gameObject.SetActive (false);
+			gameObject.transform.position = initialPosition;
+			gameObject.transform.rotation = initialRotation;
+			gameObject.SetActive (true);
+		}
+
+	}
+
+	public bool pickup(){
+		if (isPickupable) {
+			isPickupable = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public bool putDown(){
+		Debug.Log ("Put down");
+		if (!isPickupable) {
+			isPickupable = true;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject == cargoGoalSpace && !isPickupable) {
+			GameObject.Find ("Player Ship").GetComponent<ShipController> ().dropOrPickupCargo ();
+			playerAchieve ();
+		}
+	}
+
+	void playerAchieve(){
+		isAchieved = true;
+		isPickupable = false;
+		gameObject.transform.position = cargoGoalSpace.transform.position;
+		gameObject.transform.rotation = cargoGoalSpace.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- SpaceCrane/Assets/Scripts/ShipController.cs
+++ SpaceCrane/Assets/Scripts/ShipController.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    public GameObject cargo;
+    
+	public GameObject cargoCrane;
+	public GameObject initialCargo;
     public float speed;
     public float distance;
     public float zoomSpeed;
-    public float rotationSpeed;
 
+	private GameObject cargo;
     private Rigidbody rb;
+	private LineRenderer lr;
     private Vector3 offset;
     private float xR;
     private float yR;
     private float zR;
+	private bool isHoldingCargo;
 
     // Use this for initialization
     void Start () {
+		//true for development. Eventually cargo should be private and null for init
+		cargo = initialCargo;
+		cargo.GetComponent<CargoController> ().pickup();
+		isHoldingCargo = true;
+
         offset = -1*(transform.position - cargo.transform.position);
         xR = 0;
         yR = 0;
         zR = 0;
         rb = GetComponent<Rigidbody>();
+		lr = cargoCrane.GetComponent<LineRenderer> ();
     }
 	
 	// Update is called once per frame
@@ -32,28 +42,37 @@ public class ShipController : MonoBehaviour
         float moveZ = Input.GetAxis("ZAxis");
         float roll = Input.GetAxis("Roll");
 
-        //ship movement
         Vector3 movementH = transform.right * moveH;
         Vector3 movementV = transform.forward * moveV;
         Vector3 movementZ = transform.up * moveZ;
         rb.velocity = (movementH + movementV + movementZ) * speed * Time.deltaTime;
 
-        zR = roll;
+        zR += roll;
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			dropOrPickupCargo ();
+		}
 
         RaycastHit hit;
         Physics.Raycast(transform.position, transform.forward, out hit);
         //print("Object hit: " + hit.transform.gameObject.name);
-        if (hit.transform.gameObject != cargo)
-        {
-            print("Death");
-        }
+//        if (hit.transform.gameObject != cargo)
+//        {
+//            print("Death");
+//        }
     }
 
     void LateUpdate()
     {
-        UpdateCargoPosition(); // cargo movement (x and y rotation)
-        transform.rotation = Quaternion.LookRotation(cargo.transform.position - transform.position, transform.up); //look at cargo
-        transform.Rotate(0, 0, zR); //roll
+        UpdateCargoPosition();
+        transform.rotation = Quaternion.LookRotation(cargo.transform.position - transform.position);
+        transform.Rotate(0, 0, zR);
+		if (isHoldingCargo) {
+			Vector3[] segment = new Vector3[2];
+			segment [0] = cargoCrane.transform.position;
+			segment [1] = cargo.transform.position;
+			lr.SetPositions (segment);
+		}
     }
 
     void UpdateCargoPosition()
@@ -62,23 +81,46 @@ public class ShipController : MonoBehaviour
         float moveH = Input.GetAxis("Vertical");
         float zoom = Input.GetAxis("Zoom");
 
-        xR = -1 * moveH * Time.deltaTime * rotationSpeed;
-        yR = moveV * Time.deltaTime * rotationSpeed;
-        //xR = Mathf.Clamp(xR, -94, 84);
-        Quaternion yRotation = Quaternion.AngleAxis(yR, transform.up);
-        Quaternion xRotation = Quaternion.AngleAxis(xR, transform.right);
+        xR -= moveH;
+        yR += moveV;
+        xR = Mathf.Clamp(xR, -94, 84);
+        Quaternion xRotation = Quaternion.AngleAxis(xR, new Vector3(1, 0, 0));
+        Quaternion yRotation = Quaternion.AngleAxis(yR, new Vector3(0, 1, 0));
         //Quaternion zRotation = Quaternion.AngleAxis(zR, new Vector3(0, 0, 1));
-        //Vector3 newOffset = new Vector3(0, 0, 1);
-        Vector3 newOffset = new Vector3();
-        newOffset = -1 * (transform.position - cargo.transform.position);
-        newOffset = yRotation * newOffset;
+        Vector3 newOffset = offset;
         newOffset = xRotation * newOffset;
+        newOffset = yRotation * newOffset;
         //newOffset = zRotation * newOffset;
         newOffset.Normalize();
         distance += zoom * zoomSpeed * Time.deltaTime;
         distance = Mathf.Clamp(distance, 5, 25);
         newOffset *= distance;
         cargo.transform.position = transform.position + newOffset;
-        //cargo.transform.rotation = Quaternion.LookRotation(-1*(cargo.transform.position - transform.position));
+        cargo.transform.rotation = Quaternion.LookRotation(-1*(cargo.transform.position - transform.position));
     }
+
+	public void dropOrPickupCargo(){
+		//if is not holding object, pick up object in crosshairs
+		//if is holding, drop cargo
+		if (!isHoldingCargo) {
+			Debug.Log ("Pickup cargo");
+			RaycastHit hit;
+			Physics.Raycast(transform.position, transform.forward, out hit);
+			GameObject newCargo = hit.transform.gameObject;
+			if (newCargo.GetComponent<CargoController> () != null) {
+				CargoController cargoCont = newCargo.GetComponent<CargoController> ();
+				if (cargoCont.pickup ()) {
+					cargo = newCargo;
+					isHoldingCargo = true;
+				}
+			}
+		} else {
+			Debug.Log (cargo.GetComponent<CargoController>());
+			if (cargo.GetComponent<CargoController> ().putDown ()) {
+				Debug.Log ("HIT");
+				cargo = initialCargo;
+				isHoldingCargo = false;
+			}
+		}
+	}
 }
