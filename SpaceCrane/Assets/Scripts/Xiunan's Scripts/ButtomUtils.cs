dfd7f46752c75d9d5617d29969d44f80215b33ec
Xiunan Yu <yuxi01@gettysburg.edu>
2017-04-22 12:41:56 +0200

    Pause menu and changes in main menu

---
+++ SpaceCrane/Assets/Scripts/Xiunan's Scripts/ButtomUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtomUtils : MonoBehaviour {
+
+    //Loads the first level 
+    public void StartPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //Loads the Main Level
+    public void MainPressed()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Exits the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ SpaceCrane/Assets/Scripts/Xiunan's Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public enum Status
+    {
+        Active,
+        Inactive
+    }
+
+    [SerializeField] GameObject UIPanel = null;
+
+    Status status;
+
+    void Start()
+    {
+        status = Status.Inactive;
+        UIPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (status == Status.Active)
+            {
+                Close();
+            }
+            else if (status == Status.Inactive)
+            {
+                Open();
+            }
+        }
+    }
+
+    void Open()
+    {
+        Time.timeScale = 0;
+        status = Status.Active;
+        UIPanel.SetActive(true);
+    }
+
+    public void Close()
+    {
+        Time.timeScale = 1;
+        status = Status.Inactive;
+        UIPanel.SetActive(false);
+    }
+}
+
