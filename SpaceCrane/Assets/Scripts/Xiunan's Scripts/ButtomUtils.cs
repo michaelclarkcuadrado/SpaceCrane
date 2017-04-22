e0a868c20f51e10fdc0bf1b0348ab9c596dc9de3
Xiunan Yu <yuxi01@gettysburg.edu>
2017-04-22 15:50:48 +0200

    Updated Menu

--- SpaceCrane/Assets/Scripts/Xiunan's Scripts/ButtomUtils.cs
+++ SpaceCrane/Assets/Scripts/Xiunan's Scripts/ButtomUtils.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ButtomUtils : MonoBehaviour {
 
+    public GameObject MainMenu;
+    public GameObject ControlMenu;
+
     //Loads the first level 
     public void StartPressed()
     {
@@ -15,6 +18,20 @@ public class ButtomUtils : MonoBehaviour {
     public void MainPressed()
     {
         SceneManager.LoadScene(0);
+    }
+
+    //Exits current Panel and back to main menu
+    public void ExitCurrentPanel()
+    {
+        MainMenu.SetActive(true);
+        ControlMenu.SetActive(false);
+    }
+
+    //Enters the control help panel
+    public void HelpPressed()
+    {
+        MainMenu.SetActive(false);
+        ControlMenu.SetActive(true);
     }
 
     //Exits the game
