a02a7aef09bf77b9e593b45e73ec93d29544a88d
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2017-04-24 11:09:24 +0200

    Add game controller singleton and prefab
    Should be available in every scene

---
+++ SpaceCrane/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance = null;
+	public GameObject[] cargoInLevel;
+
+	static int level = 0;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+		{
+			instance = this;
+			level = SceneManager.GetActiveScene().buildIndex;
+		}
+		else { 
+			Destroy(gameObject);
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	public void checkAllAcheived () {
+		if (cargoInLevel.Length > 0) {
+			foreach(GameObject c in cargoInLevel) {
+				if (!c.GetComponent<CargoController>().achieved()) {
+					return;
+				}
+			}
+			//if every cargo is achieved, bump level to next
+			bumpLevel();
+		}
+	}
+
+	public void bumpLevel() {
+		SceneManager.LoadScene(++level);
+	}
+
+
+}
